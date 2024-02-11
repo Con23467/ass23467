979b0ab799fa7da46fb60158d55300f9abfd6e24
Con23467 <158525060+Con23467@users.noreply.github.com>
2024-02-12 10:04:53 +1300

    Update sketch_feb8a.ino

--- sketch_feb8a/sketch_feb8a.ino
+++ sketch_feb8a/sketch_feb8a.ino
@@ -1,18 +1,19 @@
 #include <Servo.h>
 
-const byte ledPin = 13; //setting up LED to pin 13  //NZQA like constants
-const byte PAW1 = 1; //setting up top right paw to pin 4
-const byte PAW2 = 2; //setting up the top left paw to pin 2
-const byte PAW3 = 3; //setting up the bottom left paw to pin 3
-const byte PAW4 = 4; //setting up the bottom right paw to pin 4
-const byte LEFTEARPIN = 9;  //constant must be in caps
-const byte RIGHTEARPIN = 10; //setting up the constant variable of the rightear pin to pin 10
+const byte ledPin = 13;       //setting up LED to pin 13  //NZQA like constants
+const byte PAW1 = 1;          //setting up top right paw to pin 4
+const byte PAW2 = 2;          //setting up the top left paw to pin 2
+const byte PAW3 = 3;          //setting up the bottom left paw to pin 3
+const byte PAW4 = 4;          //setting up the bottom right paw to pin 4
+const byte LEFTEARPIN = 9;    //constant must be in caps
+const byte RIGHTEARPIN = 10;  //setting up the constant variable of the rightear pin to pin 10
 
-Servo leftEar; //setting up the leftear servo
-Servo rightEar; // setting up the rightear servo
+Servo leftEar;   //setting up the leftear servo
+Servo rightEar;  // setting up the rightear servo
 
+//setting up the pins as either inputs or outputs
 void setup() {
-  pinMode(ledPin, OUTPUT); //saying that the led will be outputting light
+  pinMode(ledPin, OUTPUT);  //saying that the led will be outputting light
   pinMode(PAW1, INPUT);
   pinMode(PAW2, INPUT);
   pinMode(PAW3, INPUT);
@@ -22,26 +23,31 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(PAW1) == HIGH) {
-    digitalWrite(ledPin, HIGH);
+  //if we press paw 1 then the led turns on
+  if (digitalRead(PAWPIN1) == HIGH) {
+    digitalWrite(LEDPIN, HIGH);
   }
-  if (digitalRead(PAW2) == HIGH) {
-    digitalWrite(ledPin, HIGH);
+  //when we press paw 2 it switches off the led
+  if (digitalRead(PAWPIN2) == HIGH) {
+    digitalWrite(LEDPIN, LOW);
   }
-  if (digitalRead(PAW3) == HIGH) {
-    playMusic();
+  //If paw 3 is pressed then call then do the wag ears method
+  if (digitalRead(PAWPIN3) == HIGH) {
+    wagEars();
   }
-  if (digitalRead(PAW4) == HIGH) {
-    int score = playGame();
-  }
-  int playGame() {
-    byte score = 99;
-    //simon says game
-    return sciore;
+  //If paw 4 is pressed then call blinkingLed method
+  if (digitalRead(PAWPIN4) == HIGH) {
+    blibkingLed();
   }
 }
+int playGame() {
+  byte score = 99;
+  //simon says game
+  return sciore;
+}
+}
 
-void wagEars(){
+void wagEars() {
   leftear.write(0);
   rightEar.write(0);
   leftEar.write(90);
@@ -50,10 +56,10 @@ void wagEars(){
   rightEar.write(0);
 }
 
-void blinkingLed(){
-  byte randNumber = random(1,3);
+void blinkingLed() {
+  byte randNumber = random(1, 3);
 
-  for (int i = 0; i < randNumber; i++){}
+  for (int i = 0; i < randNumber; i++) {}
   digitalWrite(LEDPIN, HIGH);
   delay(500);
   digitalWrite(LEDPIN, LOW);
