6da656d3b3d3d9381345868de679d469d89d8fba
Con23467 <158525060+Con23467@users.noreply.github.com>
2024-03-15 11:46:31 +1300

    Update _.ino

--- Assesment (Level 2) 23467/_/_.ino
+++ Assesment (Level 2) 23467/_/_.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <SparkFun_LPS25HB_Arduino_Library.h> 
+
+LPS25HB sensor; 
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+  sensor.begin(); 
+
+  if (sensor.isConnected() == false) 
+  {
+    Serial.println("Seonsor hasn't been read.");  
+  
+    while (1)
+      ;
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Serial.print("Current pressure: ");
+  Serial.print(sensor.getPressure_hPa()); 
+  Serial.print(", Current Tmeperature (°C): ");
+  Serial.println(sensor.getTemperature_degC()); 
 
+  delay(40); 
 }
