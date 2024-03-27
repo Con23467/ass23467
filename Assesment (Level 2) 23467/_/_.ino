5d3c2b536cb59390a02af5e9fa67dce4f81ea05d
Con23467 <158525060+Con23467@users.noreply.github.com>
2024-03-27 13:13:00 +1300

    Update _.ino

--- Assesment (Level 2) 23467/_/_.ino
+++ Assesment (Level 2) 23467/_/_.ino
@@ -1,16 +1,16 @@
-#include <SparkFun_LPS25HB_Arduino_Library.h>
+#include <SparkFun_LPS25HB_Arduino_Library.h>  //we are including the 3 libraries so that we can
 #include <Wire.h>
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 
 LPS25HB sensor;
-OpenLog SDcard;  //Create instance
-const String FILENAME = "rocketdata.txt";
+OpenLog SDcard;                            //Create instance/labeling the Log as SDcard
+const String FILENAME = "rocketdata.txt";  //the FILENAME is being called/saved as rocketdata.txt
 int myLog;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-  sensor.begin();
+  sensor.begin(); 
 
   if (sensor.isConnected() == false) {
     Serial.println("Seonsor hasn't been read.");
@@ -25,14 +25,14 @@ void loop() {
   Serial.print("Current pressure: ");
   Serial.print(sensor.getPressure_hPa());
   Serial.print(", Current Tmeperature (°C): ");
-  Serial.println(sensor.getTemperature_degC()); //The serial print is showing us the values on a screen 
+  Serial.println(sensor.getTemperature_degC());  //The serial print is showing us the values on a screen
 
   SDcard.print("Current pressure: ");
   SDcard.print(sensor.getPressure_hPa());
   SDcard.print(", Current Tmeperature (°C): ");
-  SDcard.println(sensor.getTemperature_degC()); // the myLog is saving the values to the SD card 
+  SDcard.println(sensor.getTemperature_degC());  // the myLog is saving the values to the SD card
 
-  delay(40); //every 40 milliseconds the task repeats itself, giving us new values.
+  delay(40);  //every 40 milliseconds the task repeats itself, giving us new values.
 }
 
 
