da8588b925e4e6f69d3bc76d588622e2192a4e8f
Con23467 <158525060+Con23467@users.noreply.github.com>
2024-05-18 00:28:36 +1200

    Updated comments

--- assessment/assessment.ino
+++ assessment/assessment.ino
@@ -6,7 +6,7 @@
 LPS25HB sensor;                            //The LPS25HB is a pressure sensor wich functions as a digital output barometer
 OpenLog SDcard;                            //Create instance/labeling the Log as SDcard
 const String FILENAME = "rocketdata.txt";  //the FILENAME is being called/saved as rocketdata.txt
-const int MYDELAY = 500;                   //setting up MYDELAY as a constant integer of 0.5 seconds
+const int MYDELAY = 500;                   //setting up MYDELAY as a constant integer of 0.5 seconds (The time span can be changed)
 
 
 void setup() {         //Initializes the serial moniter/sets up the serial moniter
@@ -42,24 +42,24 @@ void setup() {         //Initializes the serial moniter/sets up the serial monit
 }
 
 void loop() {      //the void loop is being used to loop (repeat) the code in the loop ({}) with the serial monitor printing out the pressure and temperature values. The loop is bing used to repeatedly remeausre the values.
-  launch(true);    // t=debug f=launch //add this to testyin ther true and false
+  launch(true);    // t=debug f=launch //add this to testing if they're true and false
   delay(MYDELAY);  //every 0.5 seconds (As set) so the task repeats itself, giving us new values. This can be changed when the final time incroments are deceided.
 }
 
 void launch(bool debug) {  //This is setting up my method for the launch sequence
-  if (debug == true) {     //we are saying that when our debug is true our serial monitor will output the text that is hardcoded and then the temperature and pressure which is dependent on everything outside the rockets control
+  if (debug == true) {     //we are saying that when our debug is true our serial monitor will output the text that is set and then the temperature and pressure which is dependent on everything outside the rockets control
 
     Serial.print("Current Pressure is: ");         //The serial print is printing out "Current Pressure is:" and then the following line will be outputting the current pressure
     Serial.println(sensor.getPressure_hPa());      //The serial println is is showing us the pressure values on the serial monitor, showing us what is being inputted to the sensor
-    Serial.print("Current Temperature: (°C)");     //The serial print is showing us the temperature values on the serial monitor, showing us what is being inputted
+    Serial.print("Current Temperature (°C):");     //The serial print is printing out "Current Temperature (°C):" and then the following line will be outputting the current temperature
     Serial.println(sensor.getTemperature_degC());  //The serial print is showing us the temperature values on the serial monitor, showing us what is being inputted
 
-  } else {
+  } else {  //if false then do this
 
     //the SDcard print and println is writing out the values for the pressure and the temperature then saving it to the SD card. The same things are happening in the next for lines as in the top four lines of code
     // SDcard.print("Current pressure: ");
     SDcard.print(sensor.getPressure_hPa());
-    SDcard.print(",");
+    SDcard.print(",");                             //a comma is being printed between the two values, sepertaing them and enforcing  the CSV format
     SDcard.println(sensor.getTemperature_degC());  // the SDcard is saving the values to the SD card
 
     SDcard.syncFile();  //syncing the information to the file "rocketdata.txt" sothat it is accessable and findable when I plug the SDcard into my laptop
