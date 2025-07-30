58b3d32093da4124683456c7c182e51e68755969
Con23467 <158525060+Con23467@users.noreply.github.com>
2025-07-31 09:32:49 +1200

    Update ProjectDigitech.c

--- assessment/ProjectDigitech.c
+++ assessment/ProjectDigitech.c
@@ -1,6 +1,117 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <SPI.h>
+#include "arduino_secrets.h"
 #include <ESPAsyncWebServer.h>
+#define SECRET_SSID "T8-Arduino"
+#define SECRET_PASS "T8-Arduino"
+
+
+
+const char SSID[] = SECRET_SSID;
+const char PASSWORD[] = SECRET_PASS;
+
+WifiServer server(80);
+
+const byte LEDPIN = 13;
+const byte SENSORPIN = A5;
+
+*WiFi stuff
+
+void initWiFi()
+{
+    WiFi.mode(WIFI_STA)
+    Wifi.begin(SSID, PASSWORD);
+    Serial.print('Connecting to WiFi ..');
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print('.');
+        delay(1000);
+    }
+Serial.print("Connected to ");
+Serial.println(SSID);
+
+Serial,print("Use http://");
+Serial.println(WiFi.localIP());
+}
+
+/************
+ * Setup stuff
+ ************/
+void setup()
+{
+
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(SENSORPIN, INPUT);
+
+  Serial.begin(115200);
+  delay(5000); 
+
+  initWWiFi();
+
+  server.begin();
+}
+
+/************
+ * loop stuff
+ ************/
+void loop()
+{
+
+  //check if anyone connects to the ESP32
+  WiFiClient client = server.avaliable();
+  if (client)
+  {
+    Serial.println("new client");
+    //variable to hold any incoming data from the browser or client
+    String currentLine = "";
+
+    //while they are connected
+    while (client.connected())
+    {
+      if (client.avaliable())
+      {
+        //another variable to hold any incoming
+        char c = client.read();
+        Serial.write(c);
+
+        /////////////////////////////
+        //if browser sent a newline character
+        if (c == '\n')
+        {
+          //if the current line is blank, you
+          //that's the end of the client HTTP
+          if (currentLine.length() == 0)
+          {
+            //HTTP code for a webpage after some initial set
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println("Refresh: 5"); //refresh
+            client.println();
+
+          }
+        }
+      }
+    }
+  }
+}
+
+[env:adafruit_feather_esp32s3_reversetft]
+platform = espressif32
+board = adafruit_feather_esp32s3_reversetft
+framework = arduino
+build_flags = 
+-DARDUINO_USB_MODE=1
+-DARDUINO_USB_CDC_ON_BOOT=1
+mointor_speed = 115200
+lib_compact_mode = strict 
+lib_ldf_mode = chain+
+
+
+
+
+\XSHAL_STRICT_CACHEATTR_BYPASSlib_ldf_mode = chain+
 
 int myFunction(int, int);
 
