3c5f6b03a28e7165c4ffabfa88ead2e075048932
Con23467 <158525060+Con23467@users.noreply.github.com>
2025-10-20 20:28:40 +1300

    Update arduino_secrets.h

--- assessment/src/arduino_secrets.h
+++ assessment/src/arduino_secrets.h
@@ -3,5 +3,8 @@
 
 //#define SECRET_SSID "OrewaCollege-PSK" //home wifi T8-Arduino
 //#define SECRET_PASS "P5KOrew@!" //home password
- #define SECRET_SSID "SPARK-3ANAEU" //home wifi T8-Arduino
- #define SECRET_PASS "FunkyLizard61W" //home password
+ //#define SECRET_SSID "SPARK-3ANAEU" //home wifi T8-Arduino
+ //#define SECRET_PASS "FunkyLizard61W" //home password
+
+  #define SECRET_SSID "Mangan" //home wifi T8-Arduino
+ #define SECRET_PASS "Apple123" //home password
