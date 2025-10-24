af7c6570d598b5a9e0d0fa11dc8d79f58c0593f1
Con23467 <158525060+Con23467@users.noreply.github.com>
2025-10-24 19:59:22 +1300

    Final comments
    final comments made

--- assessment/src/main.cpp
+++ assessment/src/main.cpp
@@ -4,54 +4,61 @@
 #include <arduino_secrets.h>
 #include <max6675.h>
 #include <Adafruit_ST7789.h>
-// #include <ESPAsyncWebServer.h>
+#include <ESPAsyncWebServer.h>
 
-const char SSID[] = SECRET_SSID; //the secrets "tab" allows me to easily store different networks under comments sothat i can easily and qiuickly sub the different networks in depending on my location
+const char SSID[] = SECRET_SSID; //the secrets allows me to easily store different networks and allows be to keep their identity safe
 const char PASSWORD[] = SECRET_PASS;
 
-//HAN Notes - What does this do?
+// webserver istening to port 80 for HTTP connections
 WiFiServer server(80);
 
+// Identifing the MAX6675 module input pins
 int thermoDO = GPIO_NUM_37;
 int thermoCS = GPIO_NUM_35;
 int thermoCLK = 6;
-
+// Using MAX6675 library to take the 3 inputs values from the pins and convert into celsius
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
-Adafruit_ST7789 screen = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST); //library //screen working 
+Adafruit_ST7789 screen = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST); //library //screen working
 
 /************
  * WiFi stuff
  ************/
 void initWiFi()
 {
-//HAN Notes - What does this do?
+  // Sets the WiFi mode to "station" so the ESP32 will connect to the exsisting WiFi
   WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, PASSWORD);
-  Serial.print('Connecting to WiFi ..'); //displays in my serial monitor
 
-  //HAN Notes - What does this do?
+  // Begins the WiFi connection using the SSID and PASSWORD
+  WiFi.begin(SSID, PASSWORD);
+
+  //displaying connecting to wifi in serial monitor
+  Serial.print('Connecting to WiFi ..');
+
+// Keeps checking if the ESP32 has connected to WiFi
+// While it’s not connected, it prints a dot every second to show its loading
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print('.');
     delay(1000);
   }
-  delay(5000);
-  Serial.print("Connected to "); //if it connects displays this then moves to printing the SSID after this phrase further progressing to use http://
-  Serial.println(SSID);
 
-  Serial.print("Use http://"); //when here it outputs the website ip (the http:// outputs first then the linebelow does the number sequence)
-  Serial.println(WiFi.localIP()); 
+  delay(5000); // website updating every 5 seconds
+  Serial.print("Connected to "); // printing connected to for SSID
+  Serial.println(SSID); // Printing the SSID it is connected to
+
+  Serial.print("Use http://"); // when here it outputs the website ip (the http:// outputs first then the line below does the number sequence)
+  Serial.println(WiFi.localIP()); //printing to serial monitor the local IP
 }
 
 /************
  * Setup stuff
  ************/
-//under this section its all the boring setup parts, the thermocoiuples being asigned as inputs and buzzer as output
+//under this section is all the setup parts, the MAX6675 pins being assigned and the Screen and Serial monitor being initialised
 void setup()
 {
-  
-  Serial.begin(115200);
+ 
+  Serial.begin(115200); // begining serial monitor
 
 pinMode(TFT_BACKLITE, OUTPUT);
   digitalWrite(TFT_BACKLITE, HIGH);
@@ -61,12 +68,12 @@ pinMode(TFT_BACKLITE, OUTPUT);
   digitalWrite(TFT_I2C_POWER, HIGH);
   delay(10);
 
-  // initialize screen
+  // initializing the screen
   screen.init(135, 240); // Init ST7789 240x135
   screen.setRotation(3);
-  screen.fillScreen(ST77XX_YELLOW); 
+  screen.fillScreen(ST77XX_YELLOW); // setting screen background as yellow
 
-  Serial.println(F("screen is on"));
+  Serial.println(F("screen is on")); // telling serial monitor screen is on
 
 
 
@@ -74,34 +81,38 @@ pinMode(TFT_BACKLITE, OUTPUT);
   // wait for MAX chip to stabilize
   delay(500);
 
-  //HAN notes - What does this do?
+  // Initializes the WiFi connection using the settings SSID and PASSWORD
+  // This connects the ESP32 to the local WiFi network so it can host a web server
   initWiFi();
-  
-  //HAN Notes - What does this do?
+ 
+  // Starts the web server so it can listen for and respond to client requests
+  // This allows devices on the same network to connect to the ESP32s IP address
   server.begin();
 }
 
 /************
  * loop stuff
  ************/
-//under this section we are looping the output of the serial monitor displaying the IP for the website and whenever there is a new client it pauses and states so under the serial monitor aswell
+//Under this section we have our main loop that encases our screen print, serial print and mainly our client print for our website
 void loop()
 {
-  
-  //printing on ESP32 Screen
-screen.setCursor(0, 0);
-  screen.setTextColor(ST77XX_BLACK);
-  screen.setTextSize(2);
+ 
+  // Printing on ESP32 Screen
+  screen.setCursor(0, 0);
+  screen.setTextColor(ST77XX_BLACK); // making background black
+  screen.setTextSize(2); // setting text size to 2
   screen.setTextWrap(true);
   //displays the temperature on the screen
   screen.print(F("C = "));
-  screen.print(thermocouple.readCelsius());
+  screen.print(thermocouple.readCelsius()); //printing clecius value to screen
 
-  // basic readout test, just print the current temp
+  // basic serial print the current temp
   Serial.print("C = ");
   Serial.println(thermocouple.readCelsius());
+  Serial.print("F = ");
+  Serial.println(thermocouple.readFahrenheit());
  
-  //1 sec delay for the MAX6675 to update
+  // 1 sec delay for the MAX6675 to update
   delay(1000);
 
 
@@ -138,58 +149,73 @@ screen.setCursor(0, 0);
             client.println();
 
 
-//below is the start to the webpage stating that ist in html, and then further goes on to clasify fonts and background colours
+            //below is the start to the webpage stating that ist in html, and then further goes on to clasify fonts and background colours
             client.println("<!DOCTYPE HTML>");
             client.println("<html>");                                                                                                                  // from this to next /html its the webpage
-            client.println("<style>html{font-family: Arial; background-color: white;}");   //background colour is white and standard font will be Arial                                                       
+            client.println("<style>html{font-family: Arial; background-color: white;}");   //background colour is white and standard font will be Arial                                                      
             client.println("<style>html{font-family: Arial; background-color: white;}table, th, td{border: 1px solid; boareder-collapse: collapse;}"); // only cplour if theres a
             client.println("</style>");
-            client.println("<h1>The Meat Stick<h1>");    // Heading of the web page
-            client.print("<h2>The Temperature is  </h2>"); // h2 to make the headingh smaller
-            client.print(thermocouple.readCelcius()); //printing celcius value from MAX6675 module
-            client.println("<h2> Degrees Celcius </h2>"); //stating unit
 
-            client.println("<h2 style=\"font-family: 'Great Vibes', cursive; font-size: 36px; color: #513ccc; font-style: italic;\">Connor's Meat Guide</h2>"); //this line is giving the heading "Connors Meat Guide" its font, italics and colour (Royal Blue)
+            client.println("<h1>The Meat Stick<h1>");    // Heading of the web page
+
+            // This section is to print the Current Temperature
+            client.print("<h2>The Temperature is:  </h2>"); // h2 to make the heading smaller
+            if (thermocouple.readCelsius() >= 0 && <= 100) { // Checking if in Temperature range
+            client.print(thermocouple.readCelsius()); // Printing celcius value from MAX6675 module
+            client.println("<h2> Degrees Celsius </h2>"); // Stating the unit
+            } else { // if not in temperature range then
+            client.println("<h2> Out of Temperature range | Must Be Between 0-100C </h2>"); // Not within Expected Boundaries printed to client
+            }
+
+
+            client.println("<h2 style=\"font-family: 'Great Vibes', cursive; font-size: 36px; color: #513ccc; font-style: italic;\">Troy's Meat Guide</h2>"); //this line is giving the heading "Troy's Meat Guide" its font, italics and colour (Royal Blue)
+
 
             //below is my table of meat types. Under each section it displays the Type of cook (under heading Steak Type), then under Colour - the different colours and lastly the ideal temperatures under Temperature (degrees C)
-            client.println("<table>"); //startys the table/opens the command for the table
+            client.println("<table>"); //start the table/opens the command for the table
             client.println("<tr>");
-            client.println("<th>Steak Type</th>");
-            client.println("<th>Colour</th>");
-            client.println("<th>Temperature (Degrees C)</th>");
+            client.println("<th>Steak Type</th>"); // heading for steak type
+            client.println("<th>Colour</th>"); // heading for steak colour
+            client.println("<th>Temperature (Degrees C)</th>"); // heading for steak temperature
             client.println("</tr>");
             client.println("<tr>");
-            client.println("<td>Rare</td>");
-            client.println("<td style='color:darkred;'>Blood Red</td>");
+
+            //Rare section
+            client.println("<td>Rare</td>"); //Rare heading
+            client.println("<td style='color:darkred;'>Blood Red</td>"); //Blood red heading with dark red text
             client.println("<td>50</td>");
             client.println("</tr>");
             client.println("<tr>");
 
+            //Medium Rare section
             client.println("<td>Medium Rare</td>");
             client.println("<td style='color:deeppink;'>Pink</td>");
             client.println("<td>54-57</td>");
             client.println("</tr>");
             client.println("<tr>");
 
+            //Medium section
             client.println("<td>Medium</td>");
             client.println("<td style='color:#C07050;'>Pinky Brown</td>");
             client.println("<td>58-62</td>");
             client.println("</tr>");
             client.println("<tr>");
 
+            //Medium Well section
             client.println("<td>Medium Well</td>");
             client.println("<td style='color:peru;'>Light Brown</td>");
             client.println("<td>63-68</td>");
             client.println("</tr>");
             client.println("tr>");
 
+            //Well section
             client.println("<td>Well</td>");
             client.println("<td style='color:brown;'>Brown</td>");
             client.println("<td>69</td>");
             client.println("</tr>");
             client.println("<tr>");
             client.println("</table>"); // the ("</table>") closes the table/ ends the table. Any info added now won't be added into the table
-          
+         
             //this section is before my settings page - giving basic text after the table and then assigning the Settings phrase on the website to be the signal to continue with the settings page drop down once clicked
             client.println("<!DOCTYPE html>");
             client.println("<html>");
@@ -200,12 +226,14 @@ screen.setCursor(0, 0);
 
             //this section holds all the information and background work shown (and not shown) for the settings page
             client.println("<div id='temperature-section' style='display:none; border:1px solid black; padding:10px; width:250px; margin-top:10px;'>"); //creating the box around the settings page
-            client.println("<p>Preset Temperature: <span id='current-temp'>--°C</span></p>"); //Allowing the temperature to be preset 
+            client.println("<p>Preset Temperature: <span id='current-temp'>--°C</span></p>"); //Allowing the temperature to be preset
             client.println("<label for='preset'>Set Preset Temperature:</label><br>");
             client.println("<input type='number' id='preset' min='0' max='100' placeholder='Enter °C'><br><br>"); //making the max and min range for temperature settings from 0 - 11
             client.println("<button id='set-btn'>Set</button>");//this is the set button command - once clicked the users preset te,perature will now be stored under the settings display
             client.println("<button id='close-btn'>Close</button>");//this is the close button on the settings page, once clicked the settings page disappears
             client.println("</div>");
+
+  //This is the JavaScript for the Settings Tab and Functions
             client.println(R"rawliteral(
 <script>
 //in this section we are getting references to HTML elements from their Ids
@@ -227,7 +255,7 @@ screen.setCursor(0, 0);
     currentTemp.innerHTML = '24°C'; // default starting value
   }
 
-  // show the settings pannel when the user clicks "Settings" ---
+  // show the settings pannel when the user clicks "Settings"
   settings.onclick = function() {
     section.style.display = 'block'; //makes temperature section avaliable
     settings.innerHTML = 'Settings Page:'; //chnages button text to indicate active state
@@ -260,35 +288,15 @@ screen.setCursor(0, 0);
     alert('Preset temperature set to ' + newTemp + '°C');
   }
 </script>
-)rawliteral");
+)rawliteral"); // This closes the JavaScript section
 
-            client.println("</body>");
-            client.println("</html>");
-
-            break; // leave the while loop
-          }
-          else
-          { // if you got a first newline, then clear currentLine variable }
-            currentLine = "";
-          }
-        }
-        else if (c != '\r')
-        {
-          currentLine += c; // add to the end of the current line
-        }
-        if (currentLine.endsWith("GET /H"))
-        {
-          digitalWrite(MEATPIN, HIGH); // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L"))
-        {
-          digitalWrite(MEATPIN, LOW); // GET /L turns the LED off
-        }
+            client.println("</body>"); // Sends the closing </body> tag to the client, ending the main section of the webpage
+            client.println("</html>"); // Sends the closing </html> tag to the client, ending the entire HTML document
+           
       } // end of the client.avaliable
     } // end of while loop
-    // close the connection at the ESP32 end as
-    // the client is not connected (see while loop above)
-    client.stop();
-    Serial.println("client disconnected");
+   
+    client.stop(); // Ends the connection with the client
+    Serial.println("client disconnected"); // Print to the Serial Monitor confirming that the client has been disconnected
   } // end of if client
 }
